6bd018e63f6917d2af79980beae4c4eb3004092c
Bisher Dokkmak <bisher.d790@gmail.com>
2024-08-24 04:07:47 +0700

    [Setup] Health Bar: Setup Health Bar for player and zombie.

--- Source/FightingGame/AI/FGAICharacter.cpp
+++ Source/FightingGame/AI/FGAICharacter.cpp
@@ -11,3 +11,24 @@ AFGAICharacter::AFGAICharacter()
 	CameraBoom->DestroyComponent();
 	FollowCamera->DestroyComponent();
 }
+
+void AFGAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UpdateHealthBar();
+}
+
+void AFGAICharacter::TakeDamage(const float Damage)
+{
+	Super::TakeDamage(Damage);
+
+	UpdateHealthBar();
+}
+
+void AFGAICharacter::Heal(const float HealAmount)
+{
+	Super::Heal(HealAmount);
+
+	UpdateHealthBar();
+}
